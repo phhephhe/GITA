ee7b0a10051a0a4c80e23f069ee5c15dd9399c34
phhephhe <beridzedavit07@gmail.com>
2024-06-27 21:04:09 +0400

    add calendar component

---
+++ src/app/components/calendar/calendar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatCalendarCellCssClasses, MatDatepickerModule } from '@angular/material/datepicker';
+
+@Component({
+  selector: 'dga-calendar',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [MatCardModule, MatDatepickerModule],
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.scss'
+})
+
+export class CalendarComponent {
+
+  selected: Date | null =new Date();
+
+  dateClass(date: Date): MatCalendarCellCssClasses {
+    const classes = [];
+    if (date.getDate() === 7 || date.getDate() === 14 || date.getDate() === 26) {
+      classes.push('highlighted-date');
+    }
+    return classes;
+  }
+  
+}
