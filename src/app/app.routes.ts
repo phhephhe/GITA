af91fea29f051a2e4256adc4fa61e057a226eb18
phhephhe <beridzedavit07@gmail.com>
2024-06-27 21:01:40 +0400

    add router outlet & header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'job-experience', pathMatch:'full' },
+    { path: 'job-experience', loadComponent: () => import('./components/job-experience/job-experience.component').then(m => m.JobExperienceComponent) },
+    { path: 'movies', loadComponent: () => import('./components/movies/movies.component').then(m => m.MoviesComponent) },
+    { path: 'date', loadComponent: () => import('./components/date/date.component').then(m => m.DateComponent) },
+    { path: 'calendar', loadComponent: () => import('./components/calendar/calendar.component').then(m => m.CalendarComponent) },
+    { path: 'matcher-indicator', loadComponent: () => import('./components/matcher-indicator-parent/matcher-indicator-parent.component').then(m => m.MatcherIndicatorParentComponent) },
+];
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'dga-header',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
